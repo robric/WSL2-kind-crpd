8ba47809a907ae5c63705a3acfb669eb53210280
roberts richard <rroberts@juniper.net>
2022-12-07 16:57:28 +0100

    adding script

---
+++ code/kind-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Automates the installation for kind with docker/kubectl
+#
+
+apt -y install lsb-release gnupg apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg
+add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update
+apt install docker-ce docker-ce-cli containerd.io
